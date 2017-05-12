4f34e8a443e15b0ba5bebed94e183c36273bc82b
slaughtr <dslaughtr@gmail.com>
2017-05-12 12:02:41 -0700

    Import angular2-firebase, create service to use, inject into module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { HttpModule } from '@angular/http'
 
-import { MaterializeModule } from 'angular2-materialize';
+import { MaterializeModule } from 'angular2-materialize'
 
-import { AppComponent } from './app.component';
-import { MemberListComponent } from './member-list/member-list.component';
-import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { DatabaseService } from './database.service'
+
+import { AppComponent } from './app.component'
+import { MemberListComponent } from './member-list/member-list.component'
+import { MemberDetailComponent } from './member-detail/member-detail.component'
+
+import { masterFirebaseConfig } from './api-keys'
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireDatabaseModule } from 'angularfire2/database'
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+}
 
 @NgModule({
   declarations: [
@@ -19,9 +32,11 @@ import { MemberDetailComponent } from './member-detail/member-detail.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterializeModule
+    MaterializeModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
   ],
-  providers: [],
+  providers: [DatabaseService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/database.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core'
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database'
+
+
+@Injectable()
+export class DatabaseService {
+  dogs: FirebaseListObservable<any[]>
+
+  constructor(private db: AngularFireDatabase) {
+    this.dogs = db.list('/dogs')
+   }
+
+   getDogs() {
+     return this.dogs
+   }
+
+   getDogById(dogId: string) {
+     return this.db.object('/dogs/'+dogId)
+   }
+
+}
