3e417fd2254b62d7763cd314ec40f8b9b212c038
slaughtr <dslaughtr@gmail.com>
2017-05-12 14:28:47 -0700

    Add styling, implement member list route

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,11 +1,16 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component'
+import { MemberListComponent } from './member-list/member-list.component'
 
 const appRoutes: Routes = [
   {
     path: '',
     component: WelcomePageComponent
+  },
+  {
+    path: 'members',
+    component: MemberListComponent
   }
 ];
 
