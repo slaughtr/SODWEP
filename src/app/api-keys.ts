010ba7210981b946e641763e5f39175e979e7b16
slaughtr <dslaughtr@gmail.com>
2017-05-12 19:29:06 -0700

    Delete api-keys k?

--- src/app/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export const masterFirebaseConfig = {
-    apiKey: "AIzaSyA2HOVIocwgKgVgVd2v6FO0jg4Y4A53sYs",
-    authDomain: "putkeyingitignoredumbass.firebaseapp.com",
-    databaseURL: "https://putkeyingitignoredumbass.firebaseio.com",
-    projectId: "putkeyingitignoredumbass",
-    storageBucket: "putkeyingitignoredumbass.appspot.com",
-    messagingSenderId: "886845009009"
-  }
